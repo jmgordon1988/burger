cfb68eaf6cd6ecfa8dcf5b49e7320d8c1acf7bb1
John Gordon <jmgordon1988@gmail.com>
2019-01-18 09:56:56 -0800

    added connection

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,25 @@
 var mysql = require("mysql");
-
-var connection = mysql.createConnection({
+var connectionInfo;
+if (process.env.JAWSDB_URL) {
+  connectionInfo = process.env.JAWSDB_URL;
+} else {
+  connectionInfo = {
+    host: process.env,
     port: 3306,
-    host: "localhost",
     user: "root",
     password: "root",
     database: "burgers_db"
-});
+  };
+}
+var connection = mysql.createConnection(connectionInfo);
 
-connection.connect(function (err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id: " + connection.threadId);
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
 });
 
 module.exports = connection;
